6f3e78d8808c22d860f2c039688e79bfbe4ff20b
Matthieu Durut <matthieu.durut@lokad.com>
2013-12-17 20:59:31 +0100

    Adding a comment in ArithmeticMethods.h

--- TP1/ArithmeticMethods.h
+++ TP1/ArithmeticMethods.h
@@ -1,7 +1,9 @@
 bool IsOdd(int);
 
 bool IsPrime(unsigned int);
+//Pass the argument of the function by reference.
 // Is prime checks if a positive integer is a prime number. Returns true if the input is prime and false if not.
+//Pass the argument of the function by reference.
 int Factorial(unsigned int);
 
 int FactorialNoRecursion(unsigned int);
